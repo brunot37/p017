bdef1e4c07d1b62b96dff81a1f94d4b850ade769
Fábio Caseiro <a56557@alunos.ipb.pt>
2025-03-05 17:41:24 +0000

    Update Login Page

--- P017/frontend/src/views/Login.jsx
+++ P017/frontend/src/views/Login.jsx
@@ -1,40 +1,34 @@
-import React from 'react';
-import './Login.css';
 
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
 
-function Login() {
+const Login = () => {
   return (
-    <div className="login-container">
-      <div className="login-left">
-      <img src="/src/assets/LogoAgenda.png" alt="Caderno" className="illustration" />
+    <div className="registration-container">
+      <div className="registration-illustration">
+        <img src="/src/assets/LogoAgenda.png" alt="Caderno" className="illustration-image" />
       </div>
-
-      <div className="login-right">
-        <a href="#" className="back-link">← Voltar Página Principal</a>
-
-        <h2>Gestão de Disponibilidade de Horários</h2>
-        <p>LOGIN PARA CONTINUAR</p>
-
+      <div className="registration-form">
+        {/* Colocando o link "Voltar à Página Principal" mais alto */}
+        <Link to="/" className="back-to-home-link">&larr; Voltar Página Principal</Link>
+        <h2 className="registration-title">Gestão de Disponibilidade de Horários</h2>
+        <p className="registration-description">REGISTRE-SE PARA CONTINUAR</p>
         <form>
-          <div className="input-group">
-            <input type="email" placeholder="exemplo@gmail.com" />
+          <div className="input-field">
+            <input type="email" placeholder="exemplo@gmail.com" required className="input-field__red" />
           </div>
-
-          <div className="input-group">
-            <input type="password" placeholder="********" />
+          <div className="input-field">
+            <input type="password" placeholder="********" required className="input-field__red" />
           </div>
-
-          <button type="submit">Login →</button>
+          <button type="submit" className="register-button red-button">Login &rarr;</button>
         </form>
-
-        <a href="#" className="forgot-password">Esqueceu a sua password?</a>
-
-        <div className="help-link">
-          <a href="#">❔ Precisa de Ajuda?</a>
-        </div>
+        <p className="help-section">
+          <Link to="/ajuda" className="help-link">Precisa de Ajuda?</Link>
+        </p>
       </div>
     </div>
   );
-}
+};
 
-export default Login;
+export default Login
